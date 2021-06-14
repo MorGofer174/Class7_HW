903365ac7a6fdacd86cd80175b0704faddd2ab79
MorGofer174 <morgofer174@gmail.com>
2021-06-14 23:41:34 +0300

    added prints

---
+++ src/Alpha.java
@@ -0,0 +1,6 @@
+public class Alpha {
+// checks if nextline is letters
+    public boolean ifAlpha (String name){
+        return name.matches("(.*)[a-zA-Z](.*)");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Singleton x = Singleton.getInstance();
 
+//new scanner + enter name
         Scanner user = new Scanner(System.in);
-        System.out.println("please enter your name");
-        String userName = user.nextLine();
-        x.addText(userName);
+        boolean h = true;
+        while (h == true){
+             System.out.println("please enter your name");
+             String userName = user.nextLine();
+             x.addText(userName);
+             Alpha S = new Alpha();
+        if (S.ifAlpha(userName) == false){
+            System.out.println("please try again");
+        user.next();
+        continue;}
+
+        }
 
 
-        System.out.println("please enter your age");
-        int age = user.nextInt();
-        x.addText(String.valueOf(age));
+// enter age
+        boolean y = true;
+        while( y == true){
+              try {System.out.println("please enter your age");
+                  int age = user.nextInt();
+                  x.addText(String.valueOf(age));
+                  user.skip("\n");
+                  break;
+           } catch (InputMismatchException e){
+                  System.out.println("please try again");
+                  user.next();
+                  continue;}}
 
-
+// enter address
         System.out.println("please enter your address");
         String userAddress = user.nextLine();
         x.addText(userAddress);
+
 
 
         System.out.println("please enter your phone number");
@@ -28,4 +49,6 @@ public class Main {
         user.close();
 
     }
+
+
 }
